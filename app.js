f04ab22dbd2ae11ee5e48aa897b6cff2fdbf9f94
Dmitriy <zavoronk@mail.ru>
2023-05-02 22:17:17 +0300

    yes

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import exp from 'express';
 const app = exp();
 
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 import config from './config.js';
+
 import data from './data.js';
 
+app.use(exp.static('client'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,12 +19,16 @@ app.use(function(req, res, next) {
 });
 
 
+app.get('/', function(req,res) {                   
+    res.sendFile(__dirname +'/client/index.html');
+    console.log(__dirname + '/');
+})
+
 app.get('/prikol', function(req,res) {
     console.log(req.query);
-
     res.send(data.arr)
-
 });       
+
 
 app.listen(config.port, function() {      
     console.log('port :' + config.port);
---
+++ client/script.js
@@ -0,0 +1,27 @@
+
+const knopka = document.querySelector('.bok');
+const list = document.querySelector('.out');
+
+//let host = 'http://45.12.239.38:3000';
+let host = 'http://localhost:3000';
+
+async function zapros(url, fun){
+   let z = await fetch(url);
+   let zz = await z.json();
+   if(!fun) console.log(zz)
+   else fun(zz);
+    
+}
+
+function outCarts(d) {
+    d.forEach(element => {
+        const cart = document.createElement('div');
+        cart.classList.add('item');
+        cart.textContent = `Карта №${element}`;
+        list.append(cart);
+    });
+}
+
+knopka.addEventListener('click', () => {
+    zapros(host + '/prikol?k=10', outCarts)
+})
